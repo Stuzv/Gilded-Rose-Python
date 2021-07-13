75f38217f88209d5bf96561e14f042c0a58230e9
Stuart Cook <Stuart_Cook3@hotmail.com>
2021-07-13 12:01:34 +0100

    First attempt at simplifying

--- gilded_rose/gilded_rose.py
+++ gilded_rose/gilded_rose.py
@@ -3,22 +3,32 @@ class GildedRose(object):
     def __init__(self, items):
         self.items = items
 
+    def price_increase(self, item):
+        #increasing quality as sale time approches 
+        item.quality = item.quality + 1
+
+    def price_decrease(self, item):
+        #decreasing quality as sale time approches 
+        item.quality = item.quality - 1
+    
     def update_quality(self):
+        #reducing item quality over time except for Brie & concert
         for item in self.items:
             if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
                 if item.quality > 0:
                     if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
+                        self.price_decrease(item)
             else:
                 if item.quality < 50:
                     item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
+#                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
+#                        if item.sell_in < 11:
+#                            if item.quality < 50:
+#                                item.quality = item.quality + 1
+#                        if item.sell_in < 6:
+#                            if item.quality < 50:
+#                                item.quality = item.quality + 1
+            
             if item.name != "Sulfuras, Hand of Ragnaros":
                 item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
@@ -26,7 +36,7 @@ class GildedRose(object):
                     if item.name != "Backstage passes to a TAFKAL80ETC concert":
                         if item.quality > 0:
                             if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
+                                self.price_decrease(item)
                     else:
                         item.quality = item.quality - item.quality
                 else:
